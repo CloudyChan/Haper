46a9f06b66264414588920653c4af004aa512992
CloudyChan <107761059+CloudyChan@users.noreply.github.com>
2022-07-23 19:44:15 +0100

    404
    404 alerts

--- script.js
+++ script.js
@@ -36,8 +36,8 @@ const container = document.querySelector(".container"),
 function urlExists(url, callback) {
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
-    if (xhr.readyState === 5) {
-      callback(xhr.status < 500);
+    if (xhr.readyState === 4) {
+      callback(xhr.status < 400);
     }
   };
   xhr.open('HEAD', url);
