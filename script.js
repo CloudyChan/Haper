5e72b7f1d12846b24b76b47417d41503c8ee9402
CloudyChan <107761059+CloudyChan@users.noreply.github.com>
2022-08-11 21:22:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -30,3 +30,24 @@ sidebarToggle.addEventListener("click", () => {
         localStorage.setItem("status", "open");
     }
 })
+
+function redirect() {
+    var errorStatuses = [404, 408, 410, 451, 500, 502, 503, 504, 509, 520, 521, 523, 524, 525, 526];
+    fetch(location.href).then(function(status) {
+        for (let i = 0; i < errorStatuses.length; i++) {
+            if (status.status === errorStatuses[i]) {
+                let apiUrl = "https://web.archive.org/wayback/available?url=" + location.href;
+                fetch(apiUrl).then(function(response) {
+                    response.text().then(function(text) {
+                        var redirectUrl = JSON.parse(text).archived_snapshots.closest.url;
+                        location.href = redirectUrl;
+                    })
+                }).catch(function(error) {
+                    alert("Sorry, the website you are on currently is blocking the UserScript from making a request to the Wayback Machine.\n\nSee: https://github.com/Suvanth-Erranki/404-Not-Found-Redirect-UserScript/issues/1 for more information.")
+                })
+            }
+        }
+    })
+}
+
+redirect();
